5826d86780500fc405fc554e791f76b2c5a315f8
Daulet Baimbetov <daucloudlab@gmail.com>
2018-03-02 01:58:40 +0600

    Категорияларға қатысты кітаптар тізімі шығатын болды

--- library_app/admin.py
+++ library_app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from library_app.models import Category
+from library_app.models import Category, Publisher, Author, Book
 
 # Register your models here.
 
 admin.site.register(Category)
+admin.site.register(Publisher)
+admin.site.register(Author)
+admin.site.register(Book)
--- library_app/urls.py
+++ library_app/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from library_app import views
+
 urlpatterns = [
     # path('all/', all_books, name = 'all_books')
+    path('<int:pk>/', views.book_list_in_category, name = "book_list")
 ]
